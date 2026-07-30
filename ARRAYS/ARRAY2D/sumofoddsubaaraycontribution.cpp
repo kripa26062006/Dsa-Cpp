492f88e4e328df40bfd2a1c658989317b85aefd2
kripa_26 <kripachhajer26@gmail.com>
2026-07-30 16:56:22 +0530

    added the solution sum of odd subarray solution using contribution

---
+++ ARRAYS/ARRAY2D/sumofoddsubaaraycontribution.cpp
@@ -0,0 +1,19 @@
+int sumOddLengthSubarrays(vector<int>& arr) {
+    int n = arr.size();
+    int totalSum = 0;
+
+    for (int i = 0; i < n; i++) {
+        int left = i + 1;        // number of choices for start index
+        int right = n - i;       // number of choices for end index
+
+        int oddLeft = (left + 1) / 2;
+        int oddRight = (right + 1) / 2;
+        int evenLeft = left / 2;
+        int evenRight = right / 2;
+
+        int oddCount = oddLeft * oddRight + evenLeft * evenRight;
+        totalSum += arr[i] * oddCount;
+    }
+
+    return totalSum;
+}
