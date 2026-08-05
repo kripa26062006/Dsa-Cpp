99362893a5e5fac1ca52820cf26fb8c20dbe391d
kripa_26 <kripachhajer26@gmail.com>
2026-08-05 19:51:01 +0530

    added the solution of leetcode 137 problem

---
+++ BIT MANUPLANATION/singlenumberII leercode137.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        int n = nums.size();    
+        int result = 0;           
+
+        for (int i = 0; i < 32; i++) {
+
+            int count = 0;      
+
+            for (int j = 0; j < n; j++) {
+
+                if (nums[j] & (1 << i)) {
+                    count++;
+                }
+            }
+
+            if (count % 3 != 0) {
+                result |= (1 << i);   
+            }
+        }
+
+        return result;
+    }
+};
